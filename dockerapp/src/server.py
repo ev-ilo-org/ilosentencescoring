fcb1fb5e89a9326616669f4a1a1c49a4820b2cec
jeremythomasdavies <jeremy@gentlereader.com>
2021-11-07 21:23:50 +0000

    Some small cleanup

--- dockerapp/src/server.py
+++ dockerapp/src/server.py
@@ -1,3 +1,6 @@
+# This Code provided a RESTful access to the functions of the SentenceTransfer Modules.
+# The calling code (written in golang / will call out to this code via HTTP call. 
+
 from sentence_transformers import SentenceTransformer, util
 import hug
 from hug_middleware_cors import CORSMiddleware
@@ -7,11 +10,6 @@ from hug_middleware_cors import CORSMiddleware
 def sim(reference: str, text: str):
     """Get entities for displaCy ENT visualizer."""
     
-
-    #sentences1 = [reference]
-
-    #sentences2 = [text]
-
     embeddings1 = model.encode(reference, convert_to_tensor=True)
     embeddings2 = model.encode(text, convert_to_tensor=True)
 
@@ -22,31 +20,20 @@ def sim(reference: str, text: str):
 
 
 
+# Model used is the MSMacro version - msmarco-distilbert-base-?? 
+# on first run / the model will be downloaded from HuggingFace (there can be a small delay while this is happening) 
+
 #https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2/
 
-#model = SentenceTransformer('paraphrase-distilroberta-base-v1')
-#model = SentenceTransformer('nq-distilbert-base-v1')
-
-#great results / use this one best so far
+#great results returned for initial inputs using the -v3
 #model = SentenceTransformer('msmarco-distilbert-base-v3') # used for initial paper
 model = SentenceTransformer('msmarco-distilbert-base-v4') 
 
-
-#model = SentenceTransformer('msmarco-distilroberta-base-v2')
-
-#model = SentenceTransformer('stsb-roberta-large')
-
-
-
-
 if __name__ == "__main__":
     import waitress
-   #nlp = spacy.load('xx_use_lg')
 
-
-
-# Two lists of sentences
-
+# start - Example 
+# Two lists of sentences for an example of the similatiy (using non / REST - also proves )
 sentences1 = ['Read forms',
              'Read forms',
              'The new movie is awesome']
@@ -66,11 +53,9 @@ cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
 for i in range(len(sentences1)):
     print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
 
+# end - Example 
 
-
-
-  
     app = hug.API(__name__)
     app.http.add_middleware(CORSMiddleware(app))
-    waitress.serve(__hug_wsgi__, port=8083)
+    waitress.serve(__hug_wsgi__, port=8083) # REST endpoint opened on PORT 8083
 
